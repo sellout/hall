13ef420022de8c694509e76e0448557dce5e04ff
Greg Pfeil <greg@technomadic.org>
2024-08-20 22:41:33 -0600

    Invert flake inputs
    Treat Flaky as the source of truth.

--- flake.nix
+++ flake.nix
@@ -28,7 +28,11 @@
   }: let
     pname = "hall";
 
-    supportedSystems = flaky.lib.defaultSystems;
+    supportedSystems =
+      nixpkgs.lib.remove
+      ## NB: cborg-0.2.9.0, needed by Dhall, doesn’t compile on i686-linux.
+      flake-utils.lib.system.i686-linux
+      flaky.lib.defaultSystems;
   in
     {
       schemas = {
@@ -63,7 +67,6 @@
         builtins.listToAttrs
         (builtins.map
           (flaky.lib.homeConfigurations.example
-            pname
             self
             [
               ({pkgs, ...}: {
@@ -92,8 +95,9 @@
           });
       };
 
-      projectConfigurations =
-        flaky.lib.projectConfigurations.default {inherit pkgs self;};
+      projectConfigurations = flaky.lib.projectConfigurations.default {
+        inherit pkgs self supportedSystems;
+      };
 
       devShells =
         self.projectConfigurations.${system}.devShells
@@ -103,26 +107,11 @@
     });
 
   inputs = {
-    bash-strict-mode = {
-      inputs = {
-        flake-utils.follows = "flake-utils";
-        flaky.follows = "flaky";
-        nixpkgs.follows = "nixpkgs";
-      };
-      url = "github:sellout/bash-strict-mode";
-    };
+    ## Flaky should generally be the source of truth for its inputs.
+    flaky.url = "github:sellout/flaky";
 
-    flake-utils.url = "github:numtide/flake-utils";
-
-    flaky = {
-      inputs = {
-        bash-strict-mode.follows = "bash-strict-mode";
-        flake-utils.follows = "flake-utils";
-        nixpkgs.follows = "nixpkgs";
-      };
-      url = "github:sellout/flaky";
-    };
-
-    nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
+    bash-strict-mode.follows = "flaky/bash-strict-mode";
+    flake-utils.follows = "flaky/flake-utils";
+    nixpkgs.follows = "flaky/nixpkgs";
   };
 }
